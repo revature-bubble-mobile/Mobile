52a1ea9a215196d852f9f741fe445e6e38127c4e
Daniel J Belenski <38082687+Belesnkid@users.noreply.github.com>
2022-02-18 16:44:13 -0500

    basic store created
    created basic redux store for application, tested getting the state and accompanying reducer.

--- App.tsx
+++ App.tsx
@@ -1,34 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import 'react-native-gesture-handler';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {ThemeProvider} from 'react-native-elements';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ThemeProvider } from 'react-native-elements';
 import LoginView from './components/login/login-view';
 import HomeView from './components/home/home-view';
 import ProfileView from './components/profile/profile-view';
 import { NavigationContainer } from '@react-navigation/native';
+import AsyncStorageLib from '@react-native-async-storage/async-storage';
+import { store, actions } from './store';
+import { Provider, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 const Drawer = createDrawerNavigator();
 
 const verification = false;
 
+
 export default function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    (async () => {
+      const user = await AsyncStorageLib.getItem("user");
+      if (user) {
+        const profile = JSON.parse(user);
+        const setUser = actions.setUser(profile);
+        dispatch(setUser);
+      }
+    })();
+  }, [dispatch]);
   return (<>
-    {verification ? <LoginView /> :
-    <SafeAreaProvider>
-      <ThemeProvider>
-        <NavigationContainer>
-          <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={HomeView}/>
-            <Drawer.Screen name="Profile" component={ProfileView}/>
-          </Drawer.Navigator>
-        </NavigationContainer>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </SafeAreaProvider>
+    {!verification ? <LoginView /> :
+      <SafeAreaProvider>
+        <ThemeProvider>
+          <Provider store={store}>
+            <NavigationContainer>
+              <Drawer.Navigator>
+                <Drawer.Screen name="Home" component={HomeView} />
+                <Drawer.Screen name="Profile" component={ProfileView} />
+              </Drawer.Navigator>
+            </NavigationContainer>
+            <StatusBar style="auto" />
+          </Provider>
+        </ThemeProvider>
+      </SafeAreaProvider>
     }
-    </>);
+  </>);
 }
 
 const styles = StyleSheet.create({
---
+++ store.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction, configureStore } from "@reduxjs/toolkit";
+import Profile from "./dtos/profile";
+
+export interface User {
+    profile: Profile
+}
+
+const initialState: User = {
+    profile: {
+        pid: "",
+        firstName: "",
+        lastName: "",
+        passkey: "",
+        email: "",
+        username: "",
+        following: [],
+        followers: []
+    }
+}
+
+const slice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        setUser(state: User, action: PayloadAction<Profile>) {
+            state.profile = action.payload;
+        }
+    }
+})
+
+export const store = configureStore({
+    reducer: slice.reducer
+});
+
+export const actions = slice.actions;
