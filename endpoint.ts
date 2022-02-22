876b2cc78a58ad66b66b6183b61221066d68bf12
Walter <wrkagel@uw.edu>
2022-02-22 08:32:17 -0800

    Added missing api onto endpoint.

--- endpoint.ts
+++ endpoint.ts
@@ -1,5 +1,5 @@
 
-export const azureEndpoint = "https://wk-revature-bubble-mobile.azurewebsites.net";
+export const azureEndpoint = "https://wk-revature-bubble-mobile.azurewebsites.net/api";
 
 const endpoint = "https://bubble-app-82a5a-default-rtdb.firebaseio.com/";
 
