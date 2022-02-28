cf93bfdb38a8ed66005009d21b185c016533eb49
Walter <wrkagel@uw.edu>
2022-02-28 08:13:02 -0800

    Fixed bubble using absolute positioning.

--- components/drawer/drawer-footer.tsx
+++ components/drawer/drawer-footer.tsx
@@ -1,9 +1,19 @@
-import { Image, View } from "react-native";
+import { Image, View, StyleSheet } from "react-native";
 
 
 export default function DrawerFooter() {
 
-    return(<View style={{flex:1, alignItems:"center", justifyContent:"flex-end", paddingBottom:10}}>
-    <Image style={{width:200, height:90}} source={require("../../assets/images/bubble-logo.png")}/>
-</View>)
+    return(<View style={styles.container}>
+        <Image style={styles.image} source={require("../../assets/images/bubble-logo.png")}/>
+    </View>)
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, alignItems:"center", justifyContent:"flex-end", paddingBottom:10, paddingHorizontal:10
+    },
+    image: {
+        width:"100%",
+        height:"43%"
+    }
+})
