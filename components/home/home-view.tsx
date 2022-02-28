3edde5810cd69b51007789287f0b57baa2b318b7
Joel Kurian <joeltkurian@gmail.com>
2022-02-28 10:21:55 -0600

    Fixing Create Post integration issues

--- components/home/home-view.tsx
+++ components/home/home-view.tsx
@@ -1,25 +1,14 @@
-import { StyleSheet, View } from "react-native";
+import React from "react";
+import { View } from "react-native";
 import { CreatePost } from "../create-post/create-post";
 import PostFeedView from "./post-feed/post-feed-view";
 
 export default function HomeView() {
 
-    return (<View style={styles.container}>
-        <View style={styles.createPost}>
-            <CreatePost />
-        </View>
-        <View style={{ flex: 0.8, paddingTop: 10, paddingBottom: 5 }} >
+    return (<View style={{ height: '99%', width: '100%' }}>
+        <CreatePost />
+        <View style={{ flex: 1 }}>
             <PostFeedView />
         </View>
     </View>)
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    createPost: {
-        flex: 0.2,
-    }
-
-})
