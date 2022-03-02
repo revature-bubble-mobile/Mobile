ec1b59088f4c9d45964fcc00e4c3f0f208537bd4
akromatikus <31263630+akromatikus@users.noreply.github.com>
2022-03-02 16:24:09 -0600

    added hide logs for presentation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { LogBox, StyleSheet, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -19,6 +19,8 @@ import DrawerLogout from './components/drawer/drawer-logout';
 import DrawerFooter from './components/drawer/drawer-footer';
 
 const Drawer = createDrawerNavigator();
+
+LogBox.ignoreAllLogs()
 
 export default function App() {
   const [verification, setVerification] = useState<boolean>(false);
